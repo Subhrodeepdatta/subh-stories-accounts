1d4fb5df8b62e7b8baf2b94008eb144e68344331
Subhrodeepdatta <113930688+Subhrodeepdatta@users.noreply.github.com>
2025-11-13 05:44:57 +0530

    Create main.js

---
+++ main.js
@@ -0,0 +1,28 @@
+const { app, BrowserWindow } = require('electron');
+const path = require('path');
+
+function createWindow() {
+  const win = new BrowserWindow({
+    width: 1400,
+    height: 900,
+    webPreferences: {
+      nodeIntegration: true,
+      contextIsolation: false
+    },
+    backgroundColor: '#F5F5DC',
+    icon: path.join(__dirname, 'assets/icon.ico')
+  });
+
+  win.loadFile('index.html');
+  // win.webContents.openDevTools(); // Uncomment for debugging
+}
+
+app.whenReady().then(createWindow);
+
+app.on('window-all-closed', () => {
+  if (process.platform !== 'darwin') app.quit();
+});
+
+app.on('activate', () => {
+  if (BrowserWindow.getAllWindows().length === 0) createWindow();
+});
